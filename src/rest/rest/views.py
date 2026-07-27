f7e21ab7ec2185089c1022e487e4d301b7194afd
Bhabesh <bhabeshcse@gmail.com>
2026-07-28 01:33:55 +0530

    Implement GET/POST/todos backend endpoints against mongo

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -4,17 +4,50 @@ from rest_framework.response import Response
 from rest_framework import status
 import json, logging, os
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
+
+logger = logging.getLogger(__name__)
 
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
 
+
+def serialize_todo(todo):
+    todo['id'] = str(todo.pop('_id'))
+    return todo
+
+
 class TodoListView(APIView):
 
     def get(self, request):
-        # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
-        
+        try:
+            todos = [serialize_todo(todo) for todo in db.todos.find()]
+        except PyMongoError:
+            logger.exception("Failed to fetch todos from MongoDB")
+            return Response(
+                {"error": "Failed to fetch todos"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+        return Response(todos, status=status.HTTP_200_OK)
+
     def post(self, request):
-        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        description = request.data.get('description', '').strip()
+        if not description:
+            return Response(
+                {"error": "description is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        todo = {"description": description}
+        try:
+            result = db.todos.insert_one(todo)
+        except PyMongoError:
+            logger.exception("Failed to save todo to MongoDB")
+            return Response(
+                {"error": "Failed to save todo"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+        todo['_id'] = result.inserted_id
+        return Response(serialize_todo(todo), status=status.HTTP_201_CREATED)
 
